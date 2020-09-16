878ac35058f83c3c754dc9582282b686ebf2c0ac
fabran99 <fabran99@gmail.com>
2020-09-16 20:00:52 -0300

    fix en redis

--- lol_stats_api/helpers/redis.py
+++ lol_stats_api/helpers/redis.py
@@ -2,7 +2,8 @@ from redis import Redis
 import os
 
 db_metadata = Redis(host=os.getenv("REDIS_METADATA_HOST"),
-                    db=os.getenv("REDIS_METADATA_DB"))
-db_matchlist = Redis(db=os.getenv("REDIS_GAMELIST_DB"), decode_responses=True)
+                    db=os.getenv("REDIS_METADATA_DB"), decode_responses=True)
+db_matchlist = Redis(db=os.getenv("REDIS_GAMELIST_DB"),
+                     decode_responses=True)
 db_processed_match = Redis(db=os.getenv("REDIS_GAMEID_PROCESSED_DB"))
 db_celery = Redis(db=os.getenv("CELERY_DB"), decode_responses=True)
