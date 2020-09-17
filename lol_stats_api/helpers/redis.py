ff84be7ce4023bf3bb8b123f74c79bec110baf3e
fabran99 <fabran99@gmail.com>
2020-09-17 19:04:14 -0300

    Pongo pass a redis de prod

--- lol_stats_api/helpers/redis.py
+++ lol_stats_api/helpers/redis.py
@@ -1,8 +1,13 @@
 from redis import Redis
 import os
 
-db_metadata = Redis(host=os.getenv("REDIS_METADATA_HOST"),
-                    db=os.getenv("REDIS_METADATA_DB"), decode_responses=True)
+if os.getenv("IS_PROD"):
+    db_metadata = Redis(host=os.getenv("REDIS_METADATA_HOST"),
+                        db=os.getenv("REDIS_METADATA_DB"), password=os.getenv("REDIS_METADATA_PASSWORD"), decode_responses=True)
+else:
+    db_metadata = Redis(host=os.getenv("REDIS_METADATA_HOST"),
+                        db=os.getenv("REDIS_METADATA_DB"), decode_responses=True)
+
 db_matchlist = Redis(db=os.getenv("REDIS_GAMELIST_DB"),
                      decode_responses=True)
 db_processed_match = Redis(db=os.getenv("REDIS_GAMEID_PROCESSED_DB"))
